0221d7e0e10d8ead9ffb687465f6cfabde786a01
Siddhi Bandivdekar <bandivdekarsiddhi@gmail.com>
2023-01-14 03:00:33 +0530

    Axios fetching for emails

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
-const baseURL = "https://flipkart-email-mock.now.sh"
+const baseURL = "https://flipkart-email-mock.now.sh";
 
 const fetchEmails = async (page) => {
-    try{
-        const response = await axios.get(`${baseURL}?page=${page}`);
-        console.log(response.data)
-    }
-}
+  try {
+    const response = await axios.get(`${baseURL}?page=${page}`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
